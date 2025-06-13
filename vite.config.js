5ddd5e4e15c47799eb2e2750b459d54acb841d87
jukuren48 <info@juku-ren.jp>
2025-06-13 15:09:00 +0900

    vite.config.js 設定修正 (root: './')

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
+  root: "src", // ← ここが重要！
+  build: {
+    outDir: "../dist", // 出力先をプロジェクトルート配下に
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
